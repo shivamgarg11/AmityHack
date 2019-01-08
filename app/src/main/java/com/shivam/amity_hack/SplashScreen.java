ad9a14fc2e7bc4f499ac889d2ad1a73c5bf8cbdb
Shivam Garg <contestshivamgarg@gmail.com>
2019-01-09 03:28:13 +0530

    Video completed

--- app/src/main/java/com/shivam/amity_hack/SplashScreen.java
+++ app/src/main/java/com/shivam/amity_hack/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.shivam.amity_hack;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.MediaController;
@@ -22,6 +24,26 @@ public class SplashScreen extends AppCompatActivity {
         VideoView video=findViewById(R.id.VideoView);
         video.setVideoURI(uri);
         video.start();
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 4050);
 
     }
 }
